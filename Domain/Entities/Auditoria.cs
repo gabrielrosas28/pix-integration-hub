9b178136acc7c8bf446de25427c06e0df69080da
Joao Gabriel <joaoajux@gmail.com>
2026-05-18 23:19:30 -0300

    entidade Auditoria preenchida

--- Domain/Entities/Auditoria.cs
+++ Domain/Entities/Auditoria.cs
@@ -2,5 +2,16 @@ namespace ApiService.Domain.Entities;
 
 public class Auditoria
 {
-    public int Id { get; set; }
+    public int AuditoriaId { get; set; }
+
+    
+    public int ContaId { get; set; }
+    public DateTime HorarioRegistro { get; set; }
+    public string StatusPagamento { get; set; } = string.Empty;
+    public int TxId { get; set; }
+    public decimal Valor { get; set; }
+    public string PayloadAcao { get; set; } = string.Empty;
+    public string? PayloadConfirmacao { get; set; } = string.Empty;
+    public DateTime? DataPagamento { get; set; }
+    public DateTime? HorarioConfirmacao { get; set; }
 }
